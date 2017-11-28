fa64d3196f7a95830dce910034a08083ad6b7742
Priyanshu Verma <priyanshuverma036@gmail.com>
2017-11-28 10:34:53 +0530

    Feedback Analysis

---
+++ analysis.php
@@ -0,0 +1,265 @@
+<?php
+    header("Content-type: text/css; charset: UTF-8");
+    * {
+    box-sizing: border-box;
+    }
+
+    body {
+        font-family: Arial;
+        margin: 0 auto; /* Center website */
+        max-width: 800px; /* Max width */
+        padding: 20px;
+    }
+
+    .heading {
+        font-size: 25px;
+        margin-right: 25px;
+    }
+
+    .fa {
+        font-size: 25px;
+    }
+
+    .checked {
+        color: orange;
+    }
+
+    /* Three column layout */
+    .side {
+        float: left;
+        width: 15%;
+        margin-top: 10px;
+    }
+
+    .middle {
+        float: left;
+        width: 70%;
+        margin-top: 10px;
+    }
+
+    /* Place text to the right */
+    .right {
+        text-align: right;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+
+    /* The bar container */
+    .bar-container {
+        width: 100%;
+        background-color: #f1f1f1;
+        text-align: center;
+        color: white;
+    }
+
+    /* Individual bars */
+    .bar-5 {width: 60%; height: 18px; background-color: #4CAF50;}
+    .bar-4 {width: 30%; height: 18px; background-color: #2196F3;}
+    .bar-3 {width: 10%; height: 18px; background-color: #00bcd4;}
+    .bar-2 {width: 4%; height: 18px; background-color: #ff9800;}
+    .bar-1 {width: 15%; height: 18px; background-color: #f44336;}
+
+    /* Responsive layout - make the columns stack on top of each other instead of next to each other */
+    @media (max-width: 400px) {
+        .side, .middle {
+            width: 100%;
+        }
+        /* Hide the right column on small screens */
+        .right {
+            display: none;
+        }
+    }    
+?>
+* {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Arial;
+    margin: 0 auto; /* Center website */
+    max-width: 800px; /* Max width */
+    padding: 20px;
+}
+
+.heading {
+    font-size: 25px;
+    margin-right: 25px;
+}
+
+.fa {
+    font-size: 25px;
+}
+
+.checked {
+    color: orange;
+}
+
+/* Three column layout */
+.side {
+    float: left;
+    width: 15%;
+    margin-top: 10px;
+}
+
+.middle {
+    float: left;
+    width: 70%;
+    margin-top: 10px;
+}
+
+/* Place text to the right */
+.right {
+    text-align: right;
+}
+
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+/* The bar container */
+.bar-container {
+    width: 100%;
+    background-color: #f1f1f1;
+    text-align: center;
+    color: white;
+}
+
+/* Individual bars */
+.bar-5 {width: 60%; height: 18px; background-color: #4CAF50;}
+.bar-4 {width: 30%; height: 18px; background-color: #2196F3;}
+.bar-3 {width: 10%; height: 18px; background-color: #00bcd4;}
+.bar-2 {width: 4%; height: 18px; background-color: #ff9800;}
+.bar-1 {width: 15%; height: 18px; background-color: #f44336;}
+
+/* Responsive layout - make the columns stack on top of each other instead of next to each other */
+@media (max-width: 400px) {
+    .side, .middle {
+        width: 100%;
+    }
+    /* Hide the right column on small screens */
+    .right {
+        display: none;
+    }
+}
+@font-face{
+                font-family: 'heroimagefont';
+                src: url('centerfont.otf');
+            }
+            .font1{
+                font-family: 'heroimagefont';
+            }
+            @font-face{
+                font-family: 'crabmeal';
+                src: url('crabmeal.otf');
+            }
+            .font2{
+                font-family: 'crabmeal';
+            }
+            a:link{
+                text-decoration: none;
+            }
+            .bg{
+                background-image: url("background_new2.jpg");
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            .logo{
+                padding-top: 10px;
+            }
+            .space_links{
+                padding-top: 20px;
+            }
+            .hero-text{
+                text-align: center;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+            }
+            
+            .padding-content{
+                padding-top: 84px;
+            }
+            .padding-footer{
+                padding-top: 900px;
+            }
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                width: 160px;
+                background-color: #0B4350;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                padding: 8px 16px;
+                text-decoration: none;
+            }
+
+            /* Change the link color on hover */
+            li a:hover {
+                background-color: none;
+                color: white;
+            }
+            .content{
+                padding-right: 0px;
+            }
+            
+            button.accordion {
+                background-color: #E5E8E8;
+                color: #444;
+                cursor: pointer;
+                padding: 18px;
+                width: 100%;
+                border: none;
+                text-align: left;
+                outline: none;
+                font-size: 15px;
+                transition: 0.4s;
+            }
+            /*ACCORDIAN CSS*/
+            button.accordion.active, button.accordion:hover {
+                background-color: #E8DAEF;
+            }
+
+            button.accordion:after {
+                content: '\002B';
+                color: #777;
+                font-weight: bold;
+                float: right;
+                margin-left: 5px;
+            }
+
+            button.accordion.active:after {
+                content: "\2212";
+            }
+
+            div.panel {
+                padding: 0 18px;
+                background-color: white;
+                max-height: 0;
+                overflow: hidden;
+                transition: max-height 0.2s ease-out;
+            }
+            /*ACCORDIAN CSS END*/
+
+            th,td{
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+            }
+            
+            tr:hover{background-color:#f5f5f5}
