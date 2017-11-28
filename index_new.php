5ab2dd30698d91d6981e2616f1e45c1f1a521df1
Priyanshu Verma <priyanshuverma036@gmail.com>
2017-11-28 10:50:02 +0530

    First page

---
+++ index_new.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome | Know_Your_Course</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <style type="text/css">
+            @font-face{
+                font-family: 'heroimagefont';
+                src: url('centerfont.otf');
+            }
+            .font1{
+                font-family: 'heroimagefont';
+            }
+            @font-face{
+                font-family: 'crabmeal';
+                src: url('crabmeal.otf');
+            }
+            .font2{
+                font-family: 'crabmeal';
+            }
+            a:link{
+                text-decoration: none;
+            }
+            .bg{
+                background-image: url("background_new2.jpg");
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            .logo{
+                padding-top: 10px;
+            }
+            .space_links{
+                padding-top: 20px;
+            }
+            .hero-text{
+                text-align: center;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+            }
+            
+            .padding-content{
+                padding-top: 170px;
+            }
+            .padding-footer{
+                padding-top: 155px;
+            }
+        </style>
+</head>
+<body>
+    <div class="bg">
+        <!--HEADER-->
+        <div class="container">
+            <a href="index_new.php" style="color: white;font-family: 'Raleway', sans-serif; float: left;" class="logo"><h1>Indian Institute of Information Technology<br>Vadodara</h1></a>
+            <!--<a href="contact.php" style="float: right; color: white;" class="space_links"><h3>Contact</h3></a>
+            <a href="team.php" style="float: right; color: white;" class="space_links"><h3>Team&nbsp;&nbsp;&nbsp;</h3></a>
+            <a href="home.php" style="float: right; color: white;" class="space_links"><h3>Home&nbsp;&nbsp;&nbsp;</h3></a>-->
+        </div>
+        <!--HEADER END-->
+        <div id="content">
+            <!--Main banner image-->
+            <div id = "banner_image">
+                <div class="container">	
+                    <center>
+                        <div id="banner_content" class="padding-content">
+                            <h1 class="font1" style="color: white;">KNOW YOUR COURSE</h1>
+                            <p class="font1" style="color: white;">Student & Teacher Portal</p>
+                            <br/>
+                            <a  href="Page2_new.php"><h1 class="font2">- - GET STARTED - -</h1></a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <!--Main banner image end-->
+        </div>
+        <footer>
+            <center><p class="padding-footer" style="color: white;">Copyright &amp; Know Your Course | Indian Institute of Information Technology, Vadodara Contact: +91 9000090000 </p></center>
+        </footer>
+    </div>
+</body>
+</html>
