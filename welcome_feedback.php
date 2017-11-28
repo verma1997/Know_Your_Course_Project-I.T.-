5c2b4f015ce916d093180ad0f422860cc9140731
Priyanshu Verma <priyanshuverma036@gmail.com>
2017-11-28 11:09:00 +0530

    Successful feedback submission page

---
+++ welcome_feedback.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Feedback Submitted</title>
+        <style type="text/css">
+            a:link{
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+        <center>
+            <h1 style="color: red;">Form Successfully Submitted</h1>
+                <p><a href="courses.php">Back To Dashboard</a></p>
+            </div>
+        </center>
+    </body>
+</html>
